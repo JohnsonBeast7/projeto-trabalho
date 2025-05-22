87a069ddac0f77d85f712d3fbb73213526203f0b
JohnsonBeast7 <joaopedro.ferraresi@gmail.com>
2025-05-22 17:04:53 -0300

    começo do sistema (site)

--- public/index.php
+++ public/index.php
@@ -1,16 +1,22 @@
-
-
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Me livrando da maldição</title>
 </head>
 <body>
-    <h1><?php echo "Hello World! \u{1F30E}"; ?></h1>
+    <header>
+        <img src="johnson_logo_transparent.png" alt="Logo">
+        <h1>Sistema de Usuários</h1>
+    </header>
+    <main>
+        <p></p>
+    </main>
+    
+    
     <p>Me livrando da maldição</p>
 </body>
 </html>
